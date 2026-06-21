00d28301225ca10016f07ccbbb462ad5842465e3
WisdomHuang2020 <549746477@qq.com>
2026-06-21 21:58:25 +0800

    chore: remove CircuitEditor page and all related components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Curves from './pages/Curves'
 import Designer from './pages/Designer'
 import Report from './pages/Report'
 import { DesignProvider } from './lib/DesignContext'
-import CircuitEditor from './pages/CircuitEditor'
 
 function App() {
   return (
@@ -22,7 +21,6 @@ function App() {
             <Route path="/derivations" element={<Derivations />} />
             <Route path="/curves" element={<Curves />} />
             <Route path="/designer" element={<Designer />} />
-            <Route path="/circuit-editor" element={<CircuitEditor />} />
           </Routes>
         </Layout>
       </HashRouter>
